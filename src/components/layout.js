e87332be75dbf1a44237ce47cd4df5296f285533
Rohitadesh <adeshnagalikar2000@gmail.com>
2024-07-13 14:54:09 +0530

    data commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,7 +6,7 @@ const Layout = ({children}) =>{
 
     return (
         <div className="min-h-screen w-screen border border-black">
-            <div className="h-[4rem] w-full border border-black flex justify-center items-center">
+            <div className="h-[4rem] w-full  flex justify-center items-center">
                 <Image src={logo} alt="logo" height={140} width={200} />
             </div>
             {children}
